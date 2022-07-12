b2e36057d5170f0a4e7067b107eaf0721ce0e2df
yijie <chen-yijie@qq.com>
2022-07-12 11:49:41 +0800

    新建tcp项目

---
+++ tcp/tcp_client/src/main.rs
@@ -0,0 +1,14 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::str;
+
+fn main() {
+    let mut stream = TcpStream::connect( "localhost:3000" ).unwrap();
+
+    stream.write( "hello".as_bytes() ).unwrap();
+
+    let mut buffer = [0; 5];
+    stream.read( &mut buffer ).unwrap();
+
+    println!( "{:?}", str::from_utf8( &buffer ).unwrap() );
+}
